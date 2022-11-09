82fce8df56cc230090bdd1b491b19fb8278ddf1b
FZF-Dev <fzoltan123456@gmail.com>
2022-11-09 12:35:25 +0100

    sync

---
+++ react-and-morty/src/components/Popup.jsx
@@ -0,0 +1,42 @@
+import './Popup.css';
+
+function Popup(props) {
+    
+
+    const data = props.dataToDisplay;
+    const display = text => text ?? "unknown";
+
+    if(props.data.typeOfDataToDisplay === "character") {
+        return (
+            <div className='Popup'>
+                <div id="popup-content">
+                    <span id="popup-close-button">&times;</span>
+                    <div id="popup-name">Name: {data.name}</div>
+                    <p>
+                        Status: {display(data.status)}<br/>
+                        Species: {display(data.species)}<br/>
+                        Tpye: {display(data.type)}<br/>
+                        Gender: {display(data.gender)}<br/>
+                        Origin World: {display(data.origin.name)}<br/>
+                        Current Location: {display(data.location.name)}<br/>
+                    </p>
+                </div>
+            </div>
+        )
+    } else if (props.data.typeOfDataToDisplay === "location") {
+        return (
+            <div className='Popup'>
+                <div id="popup-content">
+                    <span id="popup-close-button">&times;</span>
+                    <div id="popup-name">Name: {data.name}</div>
+                    <p>
+                        Type: {display(data.type)}<br/>
+                        Dimension: {display(data.dimension)}<br/>
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Popup;
