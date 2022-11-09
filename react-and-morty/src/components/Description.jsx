b973ac796ab0e9c9b81f7b864e5eddd755be5b7b
BalintGeri <109275872+BalintGeri@users.noreply.github.com>
2022-11-09 19:12:33 +0100

    added easter egg :)

--- react-and-morty/src/components/Description.jsx
+++ react-and-morty/src/components/Description.jsx
@@ -1,9 +1,11 @@
 import './Description.css';
+import descriptionFunny from "./images/landingpagefunny.png";
 
 function Description() {
     return (
         <div id='description'>
             <p id="descriptionText">This is a comprehensive guide to the Rick and Morty universe brought to you by Domestos Coding™</p>
+            <img id="landingEasterEgg" src={descriptionFunny} alt="landingPic"></img>
         </div>
     );
 }
